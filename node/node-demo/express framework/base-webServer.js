5897ff7ef649e2bda146bfbd4630deb456836db2
FeiQuanwei <15652693087@163.com>
2019-08-15 17:10:02 +0800

    2019/8/15

--- node/node-demo/express framework/base-webServer.js
+++ node/node-demo/express framework/base-webServer.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-  res.send('hello world')
+app.use(express.static('public'))
+app.get('/index.html',function(req,res){
+  res.sendFile( __dirname + "/" + "index.html" );
 })
-
+app.get('/process_get',function(req,res){
+  let response;
+  response = {
+    first_name:req.query.first_name,
+    last_name:req.query.last_name
+  };
+  res.end(JSON.stringify(response));
+})
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
