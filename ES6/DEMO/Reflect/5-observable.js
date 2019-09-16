beab6fec2d675812936d6417a719fd1d13d2a6d1
FeiQuanwei <15652693087@163.com>
2019-09-16 17:37:36 +0800

    2019/9/16 17:37

--- ES6/DEMO/Reflect/5-observable.js
+++ ES6/DEMO/Reflect/5-observable.js
@@ -1,11 +1,34 @@
-const person = observable({
-  name: '张三',
-  age: 20
-});
+//初始化观察者队列
+const uniqueObserveList = new Set();
 
-function print() {
-  console.log(`${person.name}, ${person.age}`)
+//将监听回调加入队列
+const observe = fn => uniqueObserveList.add(fn);
+
+//设置Proxy代理,拦截赋值操作
+const observable =  obj => new Proxy(obj, {set});
+
+//用来拦截属性的赋值操作
+function set(target, key, value, receiver){
+	//内部调用对应的 Reflect 方法
+	const result = Reflect.set(target, key, value, receiver);
+	//额外执行观察者队列
+	uniqueObserveList.forEach( item => item() );
+	return result;
 }
 
+let data = {
+	name:'hello',
+	age:30
+};
+
+//对data属性进行监听
+const person = observable(data);
+
+function print(){
+	console.log( `${person.name}, ${person.age}` );
+}
+
+//print作为监听触发的回调函数
 observe(print);
-person.name = '李四';
+
+person.name = 'world';
