14984a7e3c436ba5ca2173fa5b47162dee253946
FeiQuanwei <15652693087@163.com>
2019-11-08 17:32:58 +0800

    2019/11/8

---
+++ vue-music/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Mheader></Mheader>
+    <Tab></Tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Mheader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'App',
+  components: {
+    Mheader, Tab
+  }
+}
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+  @import '/common/stylus/variable.styl';
+  #app
+    color:$color-theme
+</style>
---
+++ vue-music/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'babel-polyfill'
+import fastclick from 'fastclick'
+import './common/stylus/index.styl'
+Vue.config.productionTip = false
+
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
