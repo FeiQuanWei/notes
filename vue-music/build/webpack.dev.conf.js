c8d33321b2cfc6ef0e51a9699c436bd475364fd1
FeiQuanwei <15652693087@163.com>
2019-12-03 14:38:01 +0800

    2019/12/3 14:37

--- vue-music/build/webpack.dev.conf.js
+++ vue-music/build/webpack.dev.conf.js
@@ -65,6 +65,20 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         }).catch((err)=>{
           console.log(err)
         })
+      }),
+      app.get('/api/lyric',function(req,res){
+        var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+        axios.get(url,{
+          headers:{
+            referer: 'https://c.y.qq.com',
+            host:'c.y.qq.com'
+          },
+          params:req.query
+        }).then((response)=>{
+          res.json(response.data)
+        }).catch((err)=>{
+          console.log(err)
+        })
       })
     }
   },
