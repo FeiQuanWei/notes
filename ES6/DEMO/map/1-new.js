fc39348d618940985d2f6d510f7139350e5e5b47
Mr.Fei <15652693087@163.com>
2019-12-05 18:33:50 +0800

    2019/12/5 18:33

---
+++ ES6/DEMO/map/1-new.js
@@ -0,0 +1,27 @@
+let obj1 = {a: 1}, obj2 = {b: 2};
+
+const  map = new Map([
+
+    ['name', '张三'],
+
+    ['age', 18],
+
+    ['sex', '男'],
+
+    [obj1, '今天天气很好'],
+
+    [obj2, '适合敲代码'],
+
+    [[1,2], 'hhh']
+
+]);
+
+// console.log(map);
+// console.log(map.size);
+// console.log(typeof map)
+// map.set('friends', ['赵六', '力气']).set(['dog'], '小花');
+// console.log(map);
+// map.set('friends', ['赵六', '力气'])
+// console.log(map);
+// console.log(map.get('name'));
+// console.log(map.get(obj1));
---
+++ test.js
@@ -0,0 +1,15 @@
+// let asdf = 'asdf'
+// let qwer = asdf.repeat(2)
+// console.log(qwer)
+
+// let asdf = [1,2,3,4,5]
+// let qwer = asdf.fill('a',2,4)
+// console.log(qwer)
+
+let obj = {
+    name, gender,
+    logDog() {
+        console.log("My" + obj.name + "is" + obj.gender)
+    }
+} 
+obj.logDog()
